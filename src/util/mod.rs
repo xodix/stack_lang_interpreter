42f03f64cbc7e4e18337f6cbfee32eeff5fda82e
xodix <kodemode@wp.pl>
2022-05-27 11:03:31 +0200

    added compilation from path.

---
+++ src/util/mod.rs
@@ -0,0 +1,17 @@
+use std::{env::args, fs, path::Path};
+
+fn extract_path() -> String {
+    args()
+        .nth(1)
+        .expect("You need to supply the path to source code.\n\nPolang.exe <PATH_TO_SRC>\n\n")
+}
+
+/**
+Function that reads the source file by using the path given by the user.
+*/
+pub fn extract_src() -> String {
+    let path = extract_path();
+    let path = Path::new(&path);
+
+    fs::read_to_string(path).expect("Could not read the file.\n")
+}
