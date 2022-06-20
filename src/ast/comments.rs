5b2ac9709fd0683af98fa8283e51d0011fcb45bb
klaus <kodemode@wp.pl>
2022-06-20 19:31:26 +0200

    Added comments and updated readme formatting.

---
+++ src/ast/comments.rs
@@ -0,0 +1,11 @@
+pub fn ignore_multiline(src: &str, i: &mut usize) {
+    let comment_end = src.find("*/").unwrap_or(src.len());
+
+    *i += comment_end;
+}
+
+pub fn ignore_single_line(src: &str, i: &mut usize) {
+    let comment_end = src.find("\n").unwrap_or(src.len());
+
+    *i += comment_end;
+}
